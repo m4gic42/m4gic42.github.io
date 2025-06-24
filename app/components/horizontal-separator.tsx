526ff6ade4327de3145c9a9b1a2fac58017af609
m4gic42 <spalvieri.alberto@gmail.com>
2025-06-24 11:55:28 +0200

    adaptive footer and stories intro

--- app/components/horizontal-separator.tsx
+++ app/components/horizontal-separator.tsx
@@ -1,6 +1,6 @@
 export function horizontalSeparator() {
   return (
-    <div style={{ paddingTop: "2rem" }} className="horizontal-separator">
+    <div style={{ paddingTop: "0rem" }} className="horizontal-separator">
       <hr className="line-main1" />
       <hr className="line-main2" />
     </div>
--- app/stories/page.tsx
+++ app/stories/page.tsx
@@ -5,11 +5,18 @@ export default function Page() {
   return (
     <>
       <NavBar />
+          <div
+      style={{ display: "flex", flexDirection: "column", minHeight: "100%" }}>
+      <div className="main-container">
+      <div className="intro">
       I always invented stories, and lacking better methods to tell them I have
       deiced to write some of them down. I mostly write silly sci-fi and surreal
       stories, both in italian and english, but you might find also a couple of
       more introspective and realistic ones, if that's your cup of tea.
+      </div>
+      </div>
       <Footer />
+      </div>
     </>
   );
 }
